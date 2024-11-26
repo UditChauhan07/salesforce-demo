84c57789862f0d4bbd80b4dc03aed5dc5e8107e6
GauravDx01 <gaurav01.dx@gmail.com>
2024-11-26 19:17:21 +0530

    add custom quantity change

--- src/components/BrandDetails/Accordion/Accordion.js
+++ src/components/BrandDetails/Accordion/Accordion.js
@@ -16,10 +16,17 @@ const Accordion = ({ salesRepId, data, formattedData, productImage = [], product
   // console.log(productCartSchema)
   const [replaceCartProduct, setReplaceCartProduct] = useState({});
   const [showName, setShowName] = useState(false);
-  const [productDetailId, setProductDetailId] = useState(null)
-  const [msg, setMsg] = useState('');
-
-
+  const [productDetailId, setProductDetailId] = useState(null);
+  const [priceInputs, setPriceInputs] = useState({});
+  const [msg, setMsg] = useState("");
+  const onPriceChangeHander = (product, price = '0') => {
+    const sanitizedPrice = price.replace(/^0+/, '') || '0';
+    setPriceInputs((prev) => ({
+      ...prev,
+      [product.Id]: sanitizedPrice,
+    }));
+    updateProductQty(product.Id, { ...product, total: sanitizedPrice });
+  };
 
   const onQuantityChange = (element, quantity) => {
     if (!quantity) {
@@ -40,16 +47,15 @@ const Accordion = ({ salesRepId, data, formattedData, productImage = [], product
       shippingMethod: JSON.parse(localStorage.getItem("shippingMethod")),
       discount: data.discount,
       SalesRepId: salesRepId,
-
-    }
+    };
 
     let manufacturer = {
       name: element.ManufacturerName__c,
-      id: localStorage.getItem('ManufacturerId__c'),
-    }
-    let orderType = 'wholesale';
+      id: localStorage.getItem("ManufacturerId__c"),
+    };
+    let orderType = "wholesale";
     if (element?.Category__c?.toUpperCase() === "PREORDER" || element?.Category__c?.toUpperCase()?.match("EVENT")) {
-      orderType = 'pre-order'
+      orderType = "pre-order";
     }
     element.orderType = orderType;
     let discount = 0;
@@ -60,13 +66,15 @@ const Accordion = ({ salesRepId, data, formattedData, productImage = [], product
     } else {
       discount = data.discount?.margin || 0;
     }
-    let salesPrice = (+listPrice - ((discount || 0) / 100) * +listPrice).toFixed(2);
+    let salesPrice =  (+listPrice - ((discount || 0) / 100) * +listPrice).toFixed(2);
     element.price = salesPrice;
     element.qty = quantity;
+    console.log(salesPrice , "salesprice")
     // element.discount = discount;
     let cartStatus = addOrder(element, account, manufacturer);
-  }
-
+    
+  };
+   
   const orderSetting = (product, quantity) => {
     setReplaceCartModalOpen(false);
     addOrder(product, quantity, data.discount);
@@ -81,8 +89,9 @@ const Accordion = ({ salesRepId, data, formattedData, productImage = [], product
 
   const sendProductIdHandler = ({ productId, productName }) => {
     // navigate('/product/'+productName.replaceAll(" ","-").replaceAll("=","-"), { state: { productId } });
-    setProductDetailId(productId)
-  }
+    setProductDetailId(productId);
+  };
+  
   return (
     <>
       {replaceCartModalOpen ? (
@@ -91,10 +100,9 @@ const Accordion = ({ salesRepId, data, formattedData, productImage = [], product
           content={
             <div className="d-flex flex-column gap-3">
               <h2 className={`${styles.warning} `}>Warning</h2>
-              <p className={`${styles.warningContent} `} dangerouslySetInnerHTML={{ __html: msg ? msg : "Adding this item will replace </br> your current cart" }}>
-              </p>
+              <p className={`${styles.warningContent} `} dangerouslySetInnerHTML={{ __html: msg ? msg : "Adding this item will replace </br> your current cart" }}></p>
               <div className="d-flex justify-content-around ">
-                <button className={`${styles.modalButton}`} style={msg ? { width: '150px' } : {}} onClick={replaceCart}>
+                <button className={`${styles.modalButton}`} style={msg ? { width: "150px" } : {}} onClick={replaceCart}>
                   {msg ? "Replace cart" : "OK"}
                 </button>
                 <button className={`${styles.modalButton}`} onClick={() => setReplaceCartModalOpen(false)}>
@@ -133,11 +141,11 @@ const Accordion = ({ salesRepId, data, formattedData, productImage = [], product
                       categoryOrderQuantity = isCategoryCarted(key);
                     }
                     return (
-                      <CollapsibleRow title={key != "null" ? key : "No Category"} quantity={categoryOrderQuantity} key={index} index={index} >
+                      <CollapsibleRow title={key != "null" ? key : "No Category"} quantity={categoryOrderQuantity} key={index} index={index}>
                         {Object.values(formattedData)[index]?.map((value, indexed) => {
                           let cartProduct = isProductCarted(value.Id);
 
-                          let listPrice = Number(value?.usdRetail__c?.replace('$', '').replace(',', ''));
+                          let listPrice = Number(value?.usdRetail__c?.replace("$", "").replace(",", ""));
                           if (isNaN(listPrice)) {
                             listPrice = 0;
                           }
@@ -147,39 +155,82 @@ const Accordion = ({ salesRepId, data, formattedData, productImage = [], product
                           let qtyofItem = cartProduct?.items?.qty || 0;
 
                           if (value?.Category__c === "TESTER") {
-                            discount = data?.discount?.testerMargin
+                            discount = data?.discount?.testerMargin;
                           } else if (value?.Category__c === "Samples") {
-                            discount = data?.discount?.sample
+                            discount = data?.discount?.sample;
                           } else {
-                            discount = data?.discount?.margin
+                            discount = data?.discount?.margin;
                           }
-                          salesPrice = (+listPrice - (discount / 100) * +listPrice).toFixed(2)
+                          salesPrice = (+listPrice - (discount / 100) * +listPrice).toFixed(2);
                           return (
                             <tr className={`${styles.ControlTR} w-full `} key={indexed}>
-                              <td className={styles.ControlStyle} style={{ cursor: 'pointer' }}>
-                                {
-                                  value.ContentDownloadUrl ? <img src={value.ContentDownloadUrl} className="zoomInEffect" alt="img" width={50} onClick={() => sendProductIdHandler({ productId: value.Id, productName: value.Name })} /> :
-                                    !productImage.isLoaded ? <LoaderV2 /> :
-                                      productImage.images?.[value?.ProductCode] ?
-                                        productImage.images[value?.ProductCode]?.ContentDownloadUrl ?
-                                          <img src={productImage.images[value?.ProductCode]?.ContentDownloadUrl} className="zoomInEffect" alt="img" width={35} onClick={() => sendProductIdHandler({ productId: value.Id, productName: value.Name })} />
-                                          : <img src={productImage.images[value?.ProductCode]} className="zoomInEffect" alt="img" width={35} onClick={() => sendProductIdHandler({ productId: value.Id, productName: value.Name })} />
-                                        : <img src={Img1} className="zoomInEffect" alt="img" onClick={() => sendProductIdHandler({ productId: value.Id, productName: value.Name })} width={50} />
-                                }
+                              <td className={styles.ControlStyle} style={{ cursor: "pointer" }}>
+                                {value.ContentDownloadUrl ? (
+                                  <img
+                                    src={value.ContentDownloadUrl}
+                                    className="zoomInEffect"
+                                    alt="img"
+                                    width={50}
+                                    onClick={() => sendProductIdHandler({ productId: value.Id, productName: value.Name })}
+                                  />
+                                ) : !productImage.isLoaded ? (
+                                  <LoaderV2 />
+                                ) : productImage.images?.[value?.ProductCode] ? (
+                                  productImage.images[value?.ProductCode]?.ContentDownloadUrl ? (
+                                    <img
+                                      src={productImage.images[value?.ProductCode]?.ContentDownloadUrl}
+                                      className="zoomInEffect"
+                                      alt="img"
+                                      width={35}
+                                      onClick={() => sendProductIdHandler({ productId: value.Id, productName: value.Name })}
+                                    />
+                                  ) : (
+                                    <img
+                                      src={productImage.images[value?.ProductCode]}
+                                      className="zoomInEffect"
+                                      alt="img"
+                                      width={35}
+                                      onClick={() => sendProductIdHandler({ productId: value.Id, productName: value.Name })}
+                                    />
+                                  )
+                                ) : (
+                                  <img src={Img1} className="zoomInEffect" alt="img" onClick={() => sendProductIdHandler({ productId: value.Id, productName: value.Name })} width={50} />
+                                )}
                                 {/* {!productImage.isLoaded?<LoaderV2/>:productImage.images[value.ProductCode]?<img src={productImage.images[value.ProductCode]?.ContentDownloadUrl?productImage.images[value.ProductCode]?.ContentDownloadUrl:productImage.images[value.ProductCode]} alt="img" width={35} />:<img src={Img1} alt="img" />} */}
                               </td>
-                              <td className="text-capitalize linkEffect" style={{ fontSize: '13px', cursor: 'pointer' }} onMouseEnter={() => setShowName({ index: indexed, type: true })}
-                                onMouseLeave={() => setShowName({ index: indexed })} onClick={() => sendProductIdHandler({ productId: value.Id, productName: value.Name })}>
+                              <td
+                                className="text-capitalize linkEffect"
+                                style={{ fontSize: "13px", cursor: "pointer" }}
+                                onMouseEnter={() => setShowName({ index: indexed, type: true })}
+                                onMouseLeave={() => setShowName({ index: indexed })}
+                                onClick={() => sendProductIdHandler({ productId: value.Id, productName: value.Name })}
+                              >
                                 {indexed !== showName?.index && value.Name.length >= 23 ? `${value.Name.substring(0, 23)}...` : value.Name}
                               </td>
                               <td>{value.ProductCode}</td>
-                              <td>{(value.ProductUPC__c === null || value.ProductUPC__c === "n/a") ? "--" : value.ProductUPC__c}</td>
+                              <td>{value.ProductUPC__c === null || value.ProductUPC__c === "n/a" ? "--" : value.ProductUPC__c}</td>
                               <td>{value?.usdRetail__c?.includes("$") ? `$${listPrice}` : `$${Number(value.usdRetail__c).toFixed(2)}`}</td>
                               <td>
-                                <div className="d-flex">
-                                  ${salesPrice}
-                                </div>
-                              </td>
+  <div className="d-flex">
+    {qtyofItem > 0 ? (
+      <input
+        type="number"
+        value={priceInputs[value.Id] || salesPrice} // Ensure state binding
+        placeholder={Number(inputPrice).toFixed(2)}
+        className={`${styles.customPriceInput} ms-1`}
+        onChange={(e) => {
+          const newPrice = e.target.value.replace(/^0+/, '') || '0';
+          onPriceChangeHander(value, newPrice);
+        }}
+        id="limit_input"
+        maxLength={4}
+        name="limit_input"
+      />
+    ) : (
+      `$${salesPrice}`
+    )}
+  </div>
+</td>
                               <td>{value.Min_Order_QTY__c || 0}</td>
                               <td>
                                 <QuantitySelector
@@ -188,27 +239,25 @@ const Accordion = ({ salesRepId, data, formattedData, productImage = [], product
                                     if (quantity) {
                                       onQuantityChange(value, quantity);
                                     } else {
-                                      if (order?.Account?.id === localStorage.getItem("AccountId__c")&&isProductCarted(value.Id)) {
+                                      if (order?.Account?.id === localStorage.getItem("AccountId__c") && isProductCarted(value.Id)) {
                                         removeProduct(value.Id);
                                       }
                                     }
                                   }}
-                                  value={order?.Account?.id ===
-                                    localStorage.getItem("AccountId__c")
-                                    ? qtyofItem
-                                    : 0}
+                                  value={order?.Account?.id === localStorage.getItem("AccountId__c") ? qtyofItem : 0}
                                 />
                               </td>
                               <td>
-                                {" "}
-                                {order?.Account?.id ===
-                                  localStorage.getItem("AccountId__c")
-                                  ? qtyofItem > 0
-                                    ? "$" +
-                                    (inputPrice * qtyofItem).toFixed(2)
-                                    : "----"
-                                  : "----"}
-                              </td>
+  {order?.Account?.id === localStorage.getItem("AccountId__c") ? (
+    qtyofItem > 0 ? (
+      `$${(priceInputs[value.Id] || inputPrice || salesPrice) * qtyofItem}`
+    ) : (
+      "----"
+    )
+  ) : (
+    "----"
+  )}
+</td>
                             </tr>
                           );
                         })}
@@ -234,7 +283,12 @@ const Accordion = ({ salesRepId, data, formattedData, productImage = [], product
           </table>
         </div>
       </div>
-      <ProductDetails productId={productDetailId} setProductDetailId={setProductDetailId} ManufacturerId={localStorage.getItem("ManufacturerId__c")} AccountId={[localStorage.getItem("AccountId__c")]} />
+      <ProductDetails
+        productId={productDetailId}
+        setProductDetailId={setProductDetailId}
+        ManufacturerId={localStorage.getItem("ManufacturerId__c")}
+        AccountId={[localStorage.getItem("AccountId__c")]}
+      />
     </>
   );
 };
