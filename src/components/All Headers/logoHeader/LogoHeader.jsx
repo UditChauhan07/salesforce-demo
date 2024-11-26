21929255a24c242974f920a4b7de1e421de66564
NotoriousJat <nishantjat.dx@gmail.com>
2024-11-26 12:54:02 +0530

    11.25.24.15

--- src/components/All Headers/logoHeader/LogoHeader.jsx
+++ src/components/All Headers/logoHeader/LogoHeader.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo , useRef } from "react";
+import React, { useEffect, useState, useMemo, useRef } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { GetAuthData } from "../../../lib/store";
 import { getPermissions } from "../../../lib/permission";
@@ -20,17 +20,11 @@ const LogoHeader = () => {
   const [key, setKey] = useState();
   const [isLoading, setIsLoading] = useState(false);
   const searchRef = useRef(null);
-  const CartHandler = () => {
-    const { getOrderQuantity ,deleteOrder} = useCart()
-    let quantity = getOrderQuantity() ?? 0;
-    if (quantity) {
-
-      return (<Link to="/my-bag" className={`linkStyle`}> My Bag ({getOrderQuantity()})
-      </Link>)
-    } else {
-      return "My Bag(0)";
-    }
-  }
+  const { getOrderQuantity } = useCart();
+  const [cartQty, setCartQty] = useState(0);
+  useEffect(() => {
+    setCartQty(getOrderQuantity() ?? 0)
+  }, [getOrderQuantity])
   const handleInputChange = (e) => setSearchTerm(e.target.value);
   useEffect(() => {
     async function fetchPermissions() {
@@ -66,7 +60,7 @@ const LogoHeader = () => {
       setKey(accessToken);
 
       const salesRepIdz = authData?.Sales_Rep__c;
-      
+
 
       const response = await axios.post(`${originAPi}/beauty/search-accounts`, {
         searchTerm: term,
@@ -201,81 +195,81 @@ const LogoHeader = () => {
         <div className={`${styles.lapSetting} d-none-print`}>
           {/* Dashboard */}
           <p className={`m-0 w-[100px] ${styles.language} flex search-bar`}>
-              <div className="search-container" ref={searchRef}>
-                <input className="search expandright" id="searchright" type="search" placeholder="Search..." value={searchTerm} onChange={handleInputChange} />
-                <label className="button searchbutton" htmlFor="searchright">
-                  <span className="searchCode">Search...</span>
-                  <span className="mglass">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
-                      <circle cx="8.24976" cy="8.25" r="4.5" stroke="black" />
-                      <path
-                        d="M8.24976 6C7.95428 6 7.6617 6.0582 7.38872 6.17127C7.11574 6.28434 6.8677 6.45008 6.65877 6.65901C6.44983 6.86794 6.2841 7.11598 6.17103 7.38896C6.05795 7.66195 5.99976 7.95453 5.99976 8.25"
-                        stroke="black"
-                        strokeLinecap="round"
-                      />
-                      <path d="M14.9998 15L12.7498 12.75" stroke="black" strokeLinecap="round" />
-                    </svg>
-                  </span>
-                </label>
+            <div className="search-container" ref={searchRef}>
+              <input className="search expandright" id="searchright" type="search" placeholder="Search..." value={searchTerm} onChange={handleInputChange} />
+              <label className="button searchbutton" htmlFor="searchright">
+                <span className="searchCode">Search...</span>
+                <span className="mglass">
+                  <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                    <circle cx="8.24976" cy="8.25" r="4.5" stroke="black" />
+                    <path
+                      d="M8.24976 6C7.95428 6 7.6617 6.0582 7.38872 6.17127C7.11574 6.28434 6.8677 6.45008 6.65877 6.65901C6.44983 6.86794 6.2841 7.11598 6.17103 7.38896C6.05795 7.66195 5.99976 7.95453 5.99976 8.25"
+                      stroke="black"
+                      strokeLinecap="round"
+                    />
+                    <path d="M14.9998 15L12.7498 12.75" stroke="black" strokeLinecap="round" />
+                  </svg>
+                </span>
+              </label>
 
-                {/* Suggestions Dropdown */}
-                {isLoading ? (
-                  <ul className="dropdown-search"><Loading /></ul>
-                ) : suggestions.length > 0 ? (
-                  <ul className="dropdown-search">
-                    {suggestions.map((suggestion) => (
-                      <li key={suggestion.Id} onClick={() => handleSuggestionClick(suggestion.Id, suggestion.ManufacturerId__c, suggestion.type, suggestion.Id)}>
-                        <div className="suggestion-item">
-                          <div className="suggested-images">
-                            {suggestion.type === "Account_Manufacturer__c" && (
-                              <img className="search-logo" src={`\\assets\\images\\brandImage\\${suggestion.ManufacturerId__c}.png`} onError={(e) => (e.target.src = "\\assets\\images\\dummy.png")} alt="Manufacturer Logo" />
-                            )}
-                            {suggestion.type === "case" && <CustomerServiceIcon width={30} height={30} />}
-                            {suggestion.type === "account" && <FaStore />}
-                            {suggestion.type === "order" && <OrderIcon height={50} width={50} />}
+              {/* Suggestions Dropdown */}
+              {isLoading ? (
+                <ul className="dropdown-search"><Loading /></ul>
+              ) : suggestions.length > 0 ? (
+                <ul className="dropdown-search">
+                  {suggestions.map((suggestion) => (
+                    <li key={suggestion.Id} onClick={() => handleSuggestionClick(suggestion.Id, suggestion.ManufacturerId__c, suggestion.type, suggestion.Id)}>
+                      <div className="suggestion-item">
+                        <div className="suggested-images">
+                          {suggestion.type === "Account_Manufacturer__c" && (
+                            <img className="search-logo" src={`\\assets\\images\\brandImage\\${suggestion.ManufacturerId__c}.png`} onError={(e) => (e.target.src = "\\assets\\images\\dummy.png")} alt="Manufacturer Logo" />
+                          )}
+                          {suggestion.type === "case" && <CustomerServiceIcon width={30} height={30} />}
+                          {suggestion.type === "account" && <FaStore />}
+                          {suggestion.type === "order" && <OrderIcon height={50} width={50} />}
+                          {suggestion.type === "Product2" && (
+                            <img className="search-logo" src={`${suggestion.imageUrl}?oauth_token=${key}`} onError={(e) => (e.target.src = "\\assets\\images\\dummy.png")} alt="" />
+                          )}
+                        </div>
+                        <div className="suggested-content">
+                          <div className="api-name">
+                            {suggestion.type === "Account_Manufacturer__c" && suggestion.ManufacturerName__c}
+                            {suggestion.type === "account" && suggestion.Name}
+                            {suggestion.type === "order" && suggestion.PO_Number__c}
+                            {suggestion.type === "case" && suggestion.CaseNumber}
                             {suggestion.type === "Product2" && (
-                              <img className="search-logo" src={`${suggestion.imageUrl}?oauth_token=${key}`} onError={(e) => (e.target.src = "\\assets\\images\\dummy.png")} alt="" />
+                              <span
+                                className="product-name"
+                                title={suggestion.Name} // Shows full name on hover
+                              >
+                                {suggestion.Name.length > 18 ? `${suggestion.Name.substring(0, 18)}...` : suggestion.Name}
+                              </span>
                             )}
                           </div>
-                          <div className="suggested-content">
-                            <div className="api-name">
-                              {suggestion.type === "Account_Manufacturer__c" && suggestion.ManufacturerName__c}
-                              {suggestion.type === "account" && suggestion.Name}
-                              {suggestion.type === "order" && suggestion.PO_Number__c}
-                              {suggestion.type === "case" && suggestion.CaseNumber}
-                              {suggestion.type === "Product2" && (
-                                <span
-                                  className="product-name"
-                                  title={suggestion.Name} // Shows full name on hover
-                                >
-                                  {suggestion.Name.length > 18 ? `${suggestion.Name.substring(0, 18)}...` : suggestion.Name}
-                                </span>
-                              )}
-                            </div>
-                            <div className="suggested-name">
-                              {suggestion.type === "Account_Manufacturer__c" && <p className="suggestion-name">Brand</p>}
-                              {suggestion.type === "account" && <p className="suggestion-name">Store</p>}
-                              {suggestion.type === "case" && <p className="suggestion-name">Case</p>}
-                              {suggestion.type === "order" && <p className="suggestion-name">Placed Order</p>}
-                              {suggestion.type === "Product2" && !suggestion.OpportunityId && <p className="suggestion-name">Product</p>}
-                            </div>
+                          <div className="suggested-name">
+                            {suggestion.type === "Account_Manufacturer__c" && <p className="suggestion-name">Brand</p>}
+                            {suggestion.type === "account" && <p className="suggestion-name">Store</p>}
+                            {suggestion.type === "case" && <p className="suggestion-name">Case</p>}
+                            {suggestion.type === "order" && <p className="suggestion-name">Placed Order</p>}
+                            {suggestion.type === "Product2" && !suggestion.OpportunityId && <p className="suggestion-name">Product</p>}
                           </div>
                         </div>
+                      </div>
 
-                      </li>
-                    ))}
-                  </ul>
-                ) : (
-                  searchTerm && <ul className="dropdown-search">No Data Found</ul> // Show no data found message
-                )}
-              </div>
-            </p>
+                    </li>
+                  ))}
+                </ul>
+              ) : (
+                searchTerm && <ul className="dropdown-search">No Data Found</ul> // Show no data found message
+              )}
+            </div>
+          </p>
 
           <p className={`m-0  ${styles.language}`}>
             {/* {memoizedPermissions?.modules?.dashboard?.view ? ( */}
-              <Link to="/dashboard" className="linkStyle">
-                Dashboard
-              </Link>
+            <Link to="/dashboard" className="linkStyle">
+              Dashboard
+            </Link>
             {/* ) : (
               <span
                 onClick={handleRestrictedAccess}
@@ -291,14 +285,17 @@ const LogoHeader = () => {
 
           {memoizedPermissions?.modules?.order?.create ? <>
             <p className={`m-0  ${styles.language}`}>
-      <Link to="/my-bag" className={`linkStyle`}> <CartHandler />
+            <Link to="/my-bag" className="linkStyle">
+                My Bag ({cartQty})
               </Link>
             </p>
           </> :
             <p className={`m-0  ${styles.language}`} style={{ cursor: "not-allowed", color: "grey" }} onClick={handleRestrictedAccess}>
 
-              <span className={`linkStyle`} >  <CartHandler />
-              </span> 
+              <span className={`linkStyle`} >         <Link to="/my-bag" className="linkStyle">
+                My Bag ({cartQty})
+              </Link>
+              </span>
             </p>
           }
 
