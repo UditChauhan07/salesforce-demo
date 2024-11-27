2aad4334c2ab2216d59f472c9eb429b060291640
NotoriousJat <nishantjat.dx@gmail.com>
2024-11-27 17:29:26 +0530

    11.27.24.13

--- src/components/MyBagFinal/index.js
+++ src/components/MyBagFinal/index.js
@@ -34,6 +34,7 @@ function MyBagFinal({ showOrderFor }) {
   const [isLoading, setIsLoading] = useState(true);
   const [showErrorPopup, setShowErrorPopup] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
+  const { getOrderQuantity,updateProductPrice } = useCart();
   const handleNameChange = (event) => {
     const limit = 11;
     const value = event.target.value.slice(0, limit); // Restrict to 11 characters
@@ -251,11 +252,13 @@ function MyBagFinal({ showOrderFor }) {
 
 
   const OrderQuantity = () => {
-    const { getOrderQuantity } = useCart();
 
     return getOrderQuantity() || 0;
   }
-
+  const onPriceChangeHander = (productId, price = '0') => {
+    if (price == '') price = 0;
+    updateProductPrice(productId, price || 0)
+  }
 
 
   if (isOrderPlaced === 1) return <div style={{ height: "300px" }}><OrderLoader /> </div>;
@@ -450,7 +453,12 @@ function MyBagFinal({ showOrderFor }) {
                                         <span className={Styles.Span1}>
                                           {`$${listPrice}`}
                                         </span>
-                                        <span className={Styles.Span2}>${Number(salesPrice).toFixed(2)}</span>
+                                        <span className={Styles.Span2}>
+                                          {/* ${Number(salesPrice).toFixed(2)} */}
+                                          <input type="number" value={salesPrice} placeholder={Number(salesPrice).toFixed(2)} className={`ms-1 ${Styles.customPriceInput}`}
+                                  onChange={(e) => { onPriceChangeHander(ele?.Id, e.target.value < 10 ? e.target.value.replace("0", "").slice(0, 4) : e.target.value.slice(0, 4) || 0) }} id="limit_input" minLength={0} maxLength={4}
+                                  name="limit_input" />
+                                          </span>
                                       </p>
                                     </div>
                                   </div>
